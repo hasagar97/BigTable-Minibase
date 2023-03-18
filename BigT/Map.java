e2e7d2ca7d728770a3980c5297f4e9a9d6dd022f
hasagar97 <hrishikeshsagar8@gmail.com>
2023-03-18 12:30:27 -0700

    Fixed HFtest

--- BigT/Map.java
+++ BigT/Map.java
@@ -44,7 +44,7 @@ public class Map implements GlobalConst{
 
 
     /**
-        Variables for map data structure
+     Variables for map data structure
      */
 
     public static final short MAX_FIELD_COUNT = 4;
@@ -62,9 +62,11 @@ public class Map implements GlobalConst{
     */
     public Map()
     {
+        //System.out.println("");
+        //System.out.println("Map()");
         this.map = new byte[MAX_SIZE];
         this.map_offset = 0;
-        this.map_length = this.map.length;
+        this.map_length = MAX_SIZE;
     }
 
 
@@ -72,14 +74,14 @@ public class Map implements GlobalConst{
       Construct a map from a byte array.
     */
     public Map(byte[] amap, int offset) throws IOException {
-        System.out.println("amap offset");
+        //System.out.println("amap "+amap+"offset"+offset);
         this.map = amap;
         this.map_offset = offset;
         // [TODO] should the field offset be initialised with data?
         // [todo] should the offset count be set here?
-        this.map_length = this.map.length;
+//        this.map_length = this.map.length;
         initFieldOffset();
-        this.fldCnt = Convert.getShortValue(offset,this.map);
+        setFldCnt(Convert.getShortValue(offset,this.map));
     }
 
 
@@ -88,16 +90,16 @@ public class Map implements GlobalConst{
       Construct a map from another map through copy with given offset and len.
     */
     public Map(byte [] fromMap,int offset,int size) throws IOException {
-        System.out.println("from byte array, offset,size");
+        //System.out.println("from byte array,"+fromMap+" , offset:"+offset+",size"+size);
         this.map = fromMap;
 //        System.arraycopy(fromMap.map, fromMap.offset, this.map, 0, fromMap.size);
         this.map_offset = offset;
         this.map_length = size;
         initFieldOffset();
-        this.fldCnt = Convert.getShortValue(offset,this.map);
+//        setFldCnt(Convert.getShortValue(offset,this.map));
 
 
-        System.out.println("Map created with offset: "+offset+" \n");
+        //System.out.println("Map created with offset: "+offset+" \n");
         // [TODO] should the field offset be initialised with data?
         // [todo] should the offset count be set here?
     }
@@ -107,7 +109,7 @@ public class Map implements GlobalConst{
     */
     public Map(Map fromMap)
     {
-        System.out.println("from map");
+        //System.out.println("from map");
         this.map = fromMap.getMapByteArray();
         // todo should the map offset be copied?
         this.map_offset = 0;
@@ -128,19 +130,21 @@ public class Map implements GlobalConst{
     */
     public Map(int size)
     {
-        System.out.println("Map(size)");
+        //System.out.println("Map(size)");
         this.map = new byte[size];
         this.map_offset = 0;
         this.map_length = this.map.length;
+        this.fldCnt=4;
     }
 
 
     /** Copy the tuple byte array out
      *  @return  byte[], a byte array contains the tuple
-     *		the length of byte[] = length of the tuple
+     *    the length of byte[] = length of the tuple
      */
 
     public byte [] getMapByteArray(){
+        //System.out.println("getMapByteArray, offset:"+map_offset+", map length:"+map_length);
         byte [] tuplecopy = new byte [this.map_length];
         System.arraycopy(this.map, this.map_offset, tuplecopy, 0, this.map_length);
         return tuplecopy;
@@ -162,9 +166,10 @@ public class Map implements GlobalConst{
 
     public byte [] returnMapByteArray()
     {
-        byte [] tuplecopy = new byte [this.map_length];
-        System.arraycopy(this.map, this.map_offset, tuplecopy, 0, this.map_length);
-        return tuplecopy;
+//        //System.out.println("returnMapByteArray");
+//        byte [] tuplecopy = new byte [this.map_length];
+//        System.arraycopy(this.map, this.map_offset, tuplecopy, 0, this.map_length);
+        return this.map;
     }
 
 
@@ -173,6 +178,7 @@ public class Map implements GlobalConst{
      */
 
     public void setByteData(byte[] data) throws IOException {
+        //System.out.println("setByteData");
         this.map = data;
         initFieldOffset();
         fldCnt = Convert.getShortValue(0, data);
@@ -183,6 +189,10 @@ public class Map implements GlobalConst{
      */
     public int getOffset()
     {
+        //System.out.println(" getOffset:"+this.map_offset+"fldcnt: "+fldCnt);
+        for(int i=0;i<fldCnt;i++){
+            //System.out.println("i:"+i+" offset:"+fldOffset[i] );
+        }
         return this.map_offset;
     }
 
@@ -190,6 +200,7 @@ public class Map implements GlobalConst{
      */
     public void setOffset(int offset)
     {
+        //System.out.println("setOffset");
         this.map_offset = offset;
     }
 
@@ -198,6 +209,7 @@ public class Map implements GlobalConst{
      */
     public int getLength()
     {
+        //System.out.println(" getLength");
         return this.map_length;
     }
 
@@ -206,6 +218,7 @@ public class Map implements GlobalConst{
      */
     public void setLength(int length)
     {
+        //System.out.println("setLength");
         this.map_length = length;
     }
 
@@ -220,24 +233,29 @@ public class Map implements GlobalConst{
 
     public short noOfFlds()
     {
+        //System.out.println("noOfFlds");
         return fldCnt;
     }
 
 
     public void setFldCnt(short count){
+        //System.out.println("setFldCnt");
         this.fldCnt = count;
     }
 
     public short[] getFldOffset(){
+        //System.out.println("getFldOffset");
         return fldOffset;
     }
 
     public void setFldOffset(short[] fldOffset) {
+        //System.out.println("setFldOffset");
         this.fldOffset = fldOffset;
     }
 
 
     public String getStringField(short fieldNumber) throws IOException, CorruptedFieldNo {
+        //System.out.println("getStringField(short fieldNumber) ");
         if (fieldNumber == 3) {
             throw new CorruptedFieldNo(null, "MAP: Invalid field number");
         } else {
@@ -255,6 +273,7 @@ public class Map implements GlobalConst{
 
     public short[] copyFldOffset()
     {
+        //System.out.println("copyFldOffset()");
         short[] newFldOffset = new short[fldCnt + 1];
         for (int i=0; i<=fldCnt; i++) {
             newFldOffset[i] = fldOffset[i];
@@ -268,6 +287,7 @@ public class Map implements GlobalConst{
     */
     public Map copyMap(Map fromMap)
     {
+        //System.out.println("copyMap(Map fromMap)");
         byte[] tmp = fromMap.getMapByteArray();
         // TODO should I copy the length fromMap
         System.arraycopy(fromMap.map, fromMap.map_offset, this.map, this.map_offset, fromMap.map_length);
@@ -275,11 +295,12 @@ public class Map implements GlobalConst{
         return this;
     }
 
-    
+
     /***
-      Returns the row label.
-    */
+     Returns the row label.
+     */
     public java.lang.String getRowLabel() throws IOException {
+        //System.out.println("getRowLabel()");
         return Convert.getStrValue(this.fldOffset[ROW_INDEX - 1], this.map,
                 this.fldOffset[ROW_INDEX] - this.fldOffset[ROW_INDEX - 1]);
 //        return new String(map, map_offset, MAXROWLABELSIZE).trim();
@@ -289,14 +310,16 @@ public class Map implements GlobalConst{
       Set the row label.
     */
     public Map setRowLabel(java.lang.String val) throws IOException {
+        //System.out.println("setRowLabel(java.lang.String val)");
         Convert.setStrValue(val, this.fldOffset[ROW_INDEX - 1], this.map);
         return this;
     }
 
     /**
-      Returns the column label.
-    */
+     Returns the column label.
+     */
     public java.lang.String getColumnLabel() throws IOException {
+        //System.out.println(" getColumnLabel()");
         return Convert.getStrValue(this.fldOffset[COLUMN_INDEX - 1], this.map,
                 this.fldOffset[COLUMN_INDEX] - this.fldOffset[COLUMN_INDEX - 1]);
 //        return new String(map, map_offset + MAXROWLABELSIZE, MAXCOLUMNLABELSIZE).trim();
@@ -307,6 +330,7 @@ public class Map implements GlobalConst{
   Set the column label.
 */
     public Map setColumnLabel(java.lang.String val) throws IOException {
+        //System.out.println("setColumnLabel(java.lang.String val) throws IOException ");
         Convert.setStrValue(val, this.fldOffset[COLUMN_INDEX - 1], this.map);
         return this;
     }
@@ -314,10 +338,10 @@ public class Map implements GlobalConst{
 
 
     /**
-      Returns the timestamp.
-    */
+     Returns the timestamp.
+     */
     public int getTimeStamp() throws IOException {
-        System.out.println("attempting to getTimeStamp()\n");
+        //System.out.println("attempting to getTimeStamp()\n");
         return Convert.getIntValue(this.fldOffset[TIMESTAMP_INDEX - 1], this.map);
 //        return Convert.getIntValue(map_offset + MAXROWLABELSIZE + MAXCOLUMNLABELSIZE, map);
     }
@@ -326,6 +350,7 @@ public class Map implements GlobalConst{
       Set the timestamp.
     */
     public Map setTimeStamp(int val) throws IOException {
+        //System.out.println(" Map setTimeStamp(int val) throws IOException");
         Convert.setIntValue(val, this.fldOffset[TIMESTAMP_INDEX-1], map);
         return this;
     }
@@ -340,6 +365,7 @@ public class Map implements GlobalConst{
     */
     public java.lang.String getValue()
     {
+        //System.out.println("getValue()");
         return new String(map, map_offset + ROW_INDEX + COLUMN_INDEX + 4, VALUE_INDEX).trim();
     }
 
@@ -349,6 +375,7 @@ public class Map implements GlobalConst{
       Set the value.
     */
     public Map setValue(java.lang.String val) throws IOException {
+        //System.out.println("setValue(java.lang.String val) throws IOException ");
         Convert.setStrValue(val, this.fldOffset[VALUE_INDEX-1], map);
         return this;
     }
@@ -361,8 +388,8 @@ public class Map implements GlobalConst{
       Print out the map.
     */
     public void print() throws IOException {
-        System.out.println("{row_lable:"+getRowLabel() + " ,column_label" + getColumnLabel() + " ,timestamp"
-                + getTimeStamp() + " ,value:" + getValue()+"}");
+        //System.out.println("{row_lable:"+getRowLabel() + " ,column_label" + getColumnLabel() + " ,timestamp"
+        //+ getTimeStamp() + " ,value:" + getValue()+"}");
 
     }
 
@@ -372,13 +399,15 @@ public class Map implements GlobalConst{
 */
     public int size()
     {
+        //System.out.println("size()");
         return (short) (this.fldOffset[fldCnt] - this.map_offset);
 //        return map_length;
     }
-    
+
 
     public void tupleCopy(Map fromTuple)
     {
+        //System.out.println(" tupleCopy(Map fromTuple)");
         byte [] temparray = fromTuple.getMapByteArray();
         System.arraycopy(temparray, 0, this.map, this.map_offset, this.map_length);
 //       fldCnt = fromTuple.noOfFlds();
@@ -397,6 +426,7 @@ public class Map implements GlobalConst{
     */
     public Map mapSet(byte[] frommap, int offset)
     {
+        //System.out.println("mapSet(byte[] frommap, int offset)");
         System.arraycopy(frommap, offset, this.map, 0, this.map_length);
         // reseting map_offset to 0
         this.map_offset = 0;
@@ -409,17 +439,19 @@ public class Map implements GlobalConst{
     */
     public Map mapInit(byte[] amap, int offset) throws  IOException
     {
+        //System.out.println(" Map mapInit(byte[] amap, int offset)");
         this.map = amap;
         this.map_offset = offset;
         // TODO do we need to copy the map length
 //        this.map_length = this.map.length;
 //        initFieldOffset();
-//        this.fldCnt = Convert.getShortValue(offset,this.map);
+//        setFldCnt(Convert.getShortValue(offset,this.map));
         return  this;
     }
 
 
     private void initFieldOffset() throws IOException {
+        //System.out.println("initFieldOffset()");
         int ptr = this.map_offset +2;
         this.fldOffset = new short[MAX_FIELD_COUNT +1];
 
@@ -434,6 +466,7 @@ public class Map implements GlobalConst{
 
     @Override
     public String toString() {
+        //System.out.println("toString()");
         String s="";
         try {
             s= "{row_lable:"+getRowLabel() + " ,column_label" + getColumnLabel() + " ,timestamp"
@@ -447,6 +480,7 @@ public class Map implements GlobalConst{
     public void setHdr (short numFlds,  AttrType types[], short strSizes[])
             throws IOException, InvalidTypeException, InvalidMapSizeException
     {
+        //System.out.println("setHdr (short numFlds,  AttrType types[], short strSizes[])");
         if((numFlds +2)*2 > max_size)
             throw new InvalidMapSizeException(null, "TUPLE: TUPLE_TOOBIG_ERROR");
 
@@ -520,8 +554,8 @@ public class Map implements GlobalConst{
     /**
      * Convert this field into integer
      *
-     * @param	fldNo	the field number
-     * @return		the converted integer if success
+     * @param fldNo the field number
+     * @return    the converted integer if success
      *
      * @exception   IOException I/O errors
      * @exception FieldNumberOutOfBoundException Tuple field number out of bound
@@ -530,6 +564,7 @@ public class Map implements GlobalConst{
     public int getIntFld(int fldNo)
             throws IOException, FieldNumberOutOfBoundException
     {
+        //System.out.println("getIntFld(int fldNo)");
         int val;
         if ( (fldNo > 0) && (fldNo <= fldCnt))
         {
@@ -553,6 +588,7 @@ public class Map implements GlobalConst{
     public float getFloFld(int fldNo)
             throws IOException, FieldNumberOutOfBoundException
     {
+        //System.out.println("getFloFld(int fldNo)");
         float val;
         if ( (fldNo > 0) && (fldNo <= fldCnt))
         {
@@ -577,6 +613,7 @@ public class Map implements GlobalConst{
     public String getStrFld(int fldNo)
             throws IOException, FieldNumberOutOfBoundException
     {
+        //System.out.println("getStrFld(int fldNo)");
         String val;
         if ( (fldNo > 0) && (fldNo <= fldCnt))
         {
@@ -601,6 +638,7 @@ public class Map implements GlobalConst{
     public char getCharFld(int fldNo)
             throws IOException, FieldNumberOutOfBoundException
     {
+        //System.out.println("getCharFld(int fldNo)");
         char val;
         if ( (fldNo > 0) && (fldNo <= fldCnt))
         {
@@ -615,8 +653,8 @@ public class Map implements GlobalConst{
     /**
      * Set this field to integer value
      *
-     * @param	fldNo	the field number
-     * @param	val	the integer value
+     * @param fldNo the field number
+     * @param val the integer value
      * @exception   IOException I/O errors
      * @exception   FieldNumberOutOfBoundException Tuple field number out of bound
      */
@@ -624,6 +662,7 @@ public class Map implements GlobalConst{
     public Map setIntFld(int fldNo, int val)
             throws IOException, FieldNumberOutOfBoundException
     {
+        //System.out.println("setIntFld(int fldNo, int val)");
         if ( (fldNo > 0) && (fldNo <= fldCnt))
         {
             Convert.setIntValue (val, fldOffset[fldNo -1], map);
@@ -645,6 +684,7 @@ public class Map implements GlobalConst{
     public Map setFloFld(int fldNo, float val)
             throws IOException, FieldNumberOutOfBoundException
     {
+        //System.out.println("setFloFld(int fldNo, float val)");
         if ( (fldNo > 0) && (fldNo <= fldCnt))
         {
             Convert.setFloValue (val, fldOffset[fldNo -1], map);
@@ -667,6 +707,7 @@ public class Map implements GlobalConst{
     public Map setStrFld(int fldNo, String val)
             throws IOException, FieldNumberOutOfBoundException
     {
+        //System.out.println("setStrFld(int fldNo, String val)");
         if ( (fldNo > 0) && (fldNo <= fldCnt))
         {
             Convert.setStrValue (val, fldOffset[fldNo -1], map);
