a1c9b3580ef835918db61163bc64f811b6c91dad
hasagar97 <hrishikeshsagar8@gmail.com>
2023-03-19 17:28:10 -0700

    partaaayy

--- tests/phase2SortingTest.java
+++ tests/phase2SortingTest.java
@@ -19,9 +19,9 @@ public class phase2SortingTest {
         String dbpath = "phase2.test3.db";
         SystemDefs sysdef = new SystemDefs( dbpath, 5000 ,5000,"Clock");
         Heapfile f = new Heapfile("first_table5");
-        byte[] data = "Hello World!".getBytes();
-        byte[] data1 = "Hello World 1!".getBytes();
-        byte[] data2 = "Hello World 2!".getBytes();
+        byte[] data = "Hello World 3!".getBytes();
+        byte[] data1 = "Hello World 2!".getBytes();
+        byte[] data2 = "Hello World 4!".getBytes();
         byte[] data3 = "Hello World 3!".getBytes();
         byte[] data4 = "Hello World 4!".getBytes();
         byte[] data5 = "Hello World 5!".getBytes();
